23a752ef5128f4a204348df8f6b1bc3598b8a9c4
PyThagoras <31829033+PyMarc2@users.noreply.github.com>
2018-05-14 20:11:11 -0400

    Created a new type of setup
    old setup is still a file until the new setup is fully operationnal

--- setup.py
+++ setup.py
@@ -1,23 +1,82 @@
-import os
-import sys
-import unittest
+try:
+    import os
+    import sys
+    import numpy
+    import scipy
+    import matplotlib
+    import unittest
+
+except ImportError:
+    print("One of the package is missing or couldn't be loaded")
+
+# SETUP OF LINKS FOR THE PROJECT FOLDER
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), './')))
+
+# SETUP OF THE LINKS FOR THE CODE FOLDER
+codeFolderNameSetup = input("Enter the name of the folder containing your code:")
+codeFolderNameSetup = "../" + str(codeFolderNameSetup)
+codeFolderPathNameSetup = (os.path.abspath(os.path.join(os.path.dirname(__file__), codeFolderNameSetup)))
+
+if os.path.exists(codeFolderPathNameSetup):
+    sys.path.insert(0, codeFolderPathNameSetup)
+
+else:
+    flag_correctAnswerSetup = 0
+    while flag_correctAnswerSetup == 0:
+        print("folder name specified doesn't exist. Do you want to create it and add it to project PATH?")
+        userSetupPathAnswer = input()
+
+        if str(userSetupPathAnswer) == "yes":
+            flag_correctAnswerSetup = 1
+            os.makedirs(codeFolderPathNameSetup)
+
+            if os.path.exists(codeFolderPathNameSetup):
+                try:
+                    sys.path.insert(0, codeFolderPathNameSetup)
+                except Exception:
+                    print("An error occurred, please check the file name chosen.")
+
+        elif str(userSetupPathAnswer) == "no":
+            flag_correctAnswerSetup = 1
+            break
+
+        else:
+            print("Command was not recognized. Please enter 'yes' or 'no'.\n")
 
 
-class TestSetup(unittest.TestCase):
+# SETUP OF THE LINKS FOR THE TEST FOLDER
+testFolderNameSetup = input("Enter the name of the folder containing your tests:")
+testFolderNameSetup = "./" + str(testFolderNameSetup)
+testFolderPathNameSetup = (os.path.abspath(os.path.join(os.path.dirname(__file__), testFolderNameSetup)))
 
-    def testWriteInPATH(self):
-        sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), './code')))
-        print("\n\n================ Project interpreter PATH folders (for modules) ================")
-        for PathName in sys.path:
-            print(PathName)
-        print("================================================================================\n\n")
-        self.assertIn(str(os.path.abspath(os.path.join(os.path.dirname(__file__), './code'))), sys.path,
-                      "The path containing the code files was not added.")
+if os.path.exists(testFolderPathNameSetup):
+    sys.path.insert(0, testFolderPathNameSetup)
 
-    def testPathExists(self):
-        self.assertTrue(os.path.exists(str(os.path.abspath(os.path.join(os.path.dirname(__file__), './code')))),
-                        "Path does not exist, please name your folder containing the code, 'code'")
+else:
+    flag_correctAnswerSetup = 0
+    while flag_correctAnswerSetup == 0:
+        print("folder name specified doesn't exist. Do you want to create it and add it to project PATH?")
+        userSetupPathAnswer = input()
+
+        if str(userSetupPathAnswer) == "yes":
+            flag_correctAnswerSetup = 1
+            os.makedirs(testFolderPathNameSetup)
+
+            if os.path.exists(testFolderPathNameSetup):
+                try:
+                    sys.path.insert(0, testFolderPathNameSetup)
+                except Exception:
+                    print("An error occurred, please check the file name chosen.")
+
+        elif str(userSetupPathAnswer) == "no":
+            flag_correctAnswerSetup = 1
+            break
+
+        else:
+            print("Command was not recognized. Please enter 'yes' or 'no'.\n")
 
 
-if __name__ == '__main__':
-    unittest.main()
+print("\n\n================ Project interpreter PATH folders (for modules) ================")
+for setupPathName in sys.path:
+    print(setupPathName)
+print("================================================================================\n\n")
